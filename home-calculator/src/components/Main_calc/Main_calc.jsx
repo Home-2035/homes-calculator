0cd7f9759f77e676c7ec3f40a07bff72b72aa8d4
Sagynay Utralinova <sagynay.saydildina.1997@mail.ru>
2023-10-16 13:56:28 +0500

    add maincalc

---
+++ home-calculator/src/components/Main_calc/Main_calc.jsx
@@ -0,0 +1,29 @@
+import './Main_calc.css';
+import home from './../../images/homemini.png';
+import './maicalc.js'
+function Maincalc(){
+    return (
+    <>
+    
+    <h1 className='title'>Калькулятор строительства</h1>
+    <div className='maincalc'>
+        <div className='home'>
+            <img src={home} alt='Homemini'></img>
+        </div>
+        <div className='choice'>
+            
+            <form className='formcalc'>
+                
+               <center><label>Площадь дома, <p id="value"></p>м² </label><input type="range" min="1" max="1000"id="input" step="1"></input></center> 
+            </form>
+        </div>
+    </div>
+    
+
+    <script>
+        const value = document.getElementBy
+    </script>
+    </>
+    );
+}
+export default Maincalc;
---
+++ home-calculator/src/components/Main_calc/maicalc.js
@@ -0,0 +1,12 @@
+
+// function valueinput(){
+// const value=document.getElementById('value');
+// const input=document.getElementById('input');
+// value.textContent=input.textContent;
+// input.addEventListener("input",(event)=>{
+//     value.textContent=event.target.value;
+// });
+// }
+
+// export default valueinput();
+
