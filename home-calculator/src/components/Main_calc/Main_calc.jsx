fa2f65eac9ea21872add824ce616f0da3036e89e
Sagynay Utralinova <sagynay.saydildina.1997@mail.ru>
2023-10-17 13:44:31 +0500

    Fix range

--- home-calculator/src/components/Main_calc/Main_calc.jsx
+++ home-calculator/src/components/Main_calc/Main_calc.jsx
@@ -1,6 +1,28 @@
 import './Main_calc.css';
 import home from './../../images/homemini.png';
-import script from './maicalc.js';
+import { useState } from 'react';
+
+   const Slider = () => {
+    const [value, setValue] = useState(0);
+  
+    const handleSliderChange = (event) => {
+      setValue(event.target.value);
+    };
+  
+    return (
+      <div>
+       <center> <p className='title'>Площадь дома, {value} м²</p></center>
+        <input id='ran'
+          type="range"
+          min="1"
+          max="1000"
+          value={value}
+          onChange={handleSliderChange}
+        />
+      </div>
+    );
+  };
+  
 function Maincalc(){
     return (
     <>
@@ -12,13 +34,11 @@ function Maincalc(){
         </div>
         <div className='choice'>
             
-            <form className='formcalc'>
-                
-               <center><label>Площадь дома, <output id='values' for="rock" >1</output> м² </label><input type="range" min="1" max="1000" id="inputs" step="1"></input></center> 
-            </form>
-        </div>
+        <form className='formcalc'>
+            <Slider/>
+        </form>
     </div>
-    <script src={script}></script>
+    </div>
     </>
     );
 }
