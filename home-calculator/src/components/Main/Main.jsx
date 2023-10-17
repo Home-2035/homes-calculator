a33126a6975872ddf7349d0f8054c5a66082a292
vladislav1037 <vladoperation@bk.ru>
2023-10-17 13:04:16 +0500

    aboutUs

--- home-calculator/src/components/Main/Main.jsx
+++ home-calculator/src/components/Main/Main.jsx
@@ -1,21 +1,23 @@
-
+import AboutUs from './../AboutUs/AboutUs';
 import sas from './Main.module.css'
 
 let Main = (props) => {
     return (
         <>
-       
-        <div className={sas.picture}>
-            <div className={sas.heading}>О нашем проекте</div>
-            <div className={sas.line_cont}>
-                <div className={sas.linechild}></div>
+
+            <div className={sas.picture}>
+                <div className={sas.heading}>О нашем проекте</div>
+                <div className={sas.line_cont}>
+                    <div className={sas.linechild}></div>
+                </div>
+                <div className={sas.text}>Калькулятор представляет из себя интерфейс для расчёта<br />затрат стоимости дома, исходя из ваших пожеланий </div>
             </div>
-            <div className={sas.text}>Калькулятор представляет из себя интерфейс для расчёта<br/>затрат стоимости дома, исходя из ваших пожеланий </div>
-        </div>
-        <div className={sas.calc}>
-            <h1 className={sas.h1}>Калькулятор строительства</h1>
-        </div>
-       
+            <div className={sas.calc}>
+                <h1 className={sas.h1}>Калькулятор строительства</h1>
+            </div>
+
+
+            <AboutUs />
         </>
     )
 }
